d6e70df9687add72337ea1a3a7c0532f75ed3cc4
lob4 <76789117+lob4@users.noreply.github.com>
2025-09-20 22:50:26 +0200

    Add Streamlit multiplication quiz skeleton

---
+++ memory_trainer/__init__.py
@@ -0,0 +1,5 @@
+"""Core utilities for the memory trainer application."""
+
+from .quiz import MultiplicationQuestion, MultiplicationQuiz
+
+__all__ = ["MultiplicationQuestion", "MultiplicationQuiz"]
